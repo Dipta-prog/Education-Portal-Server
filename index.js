6c526b6c70de9c566dcee547931522c95cc3af30
Ubaidur Rahman <ubaidurrahmanmcc@gmail.com>
2021-09-27 01:48:36 +0600

    Connect with mongodb successfully

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 //const MongoClient = require('mongodb').MongoClient;
 const todoHandler = require("./routeHandler/todoHandler");
+require('dotenv').config()
+
 
 // declier the port
 const port = 1000;
@@ -11,21 +13,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// database connection with mongose
-// const uri =
-//   " mongodb+srv://mongoosePractice:ArifulIslamRaju000@cluster0.yaeov.mongodb.net/mongoosePrac?retryWrites=true&w=majority";
-// mongoose
-//   .connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("connected succesfully"))
-//   .catch((err) => console.log(err));
+// database connection with mongoose
+const uri =
+  `mongodb+srv://DevZone:${process.env.DB_PASS}@cluster0.cbiot.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected successfully"))
+  .catch((err) => console.log(err));
 
 // application routes
 app.use("/todo", todoHandler);
 
-// function err handelar
+// function err handler
 function errHandelar(err, req, res, next) {
   if (res.headersSet) {
     return next(err);
