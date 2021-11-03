1f38fcfae164543f114dd484d42d8b5bbb4b4f6c
raju10 <trolltube272@gmail.com>
2021-11-03 21:49:55 +0600

    student schema & handler part work

--- routeHandler/teacherCourseDataHandler.js
+++ routeHandler/teacherCourseDataHandler.js
@@ -3,39 +3,42 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const teacherCourseDataSchema = require("../schemas/teacherCourseDataSchema");
 
-const teacherCourseData = new mongoose.model("teacherCourseData", teacherCourseDataSchema);
+const teacherCourseData = new mongoose.model(
+  "teacherCourseData",
+  teacherCourseDataSchema
+);
 
 //post all the courses
 router.post("/addCourseDataByTeacher", (req, res) => {
-    const tempTeacherCourseData = new teacherCourseData(req.body);
-    tempTeacherCourseData.save((err) => {
-      if (err) {
-        res.status(500).json({
-          error: err,
-          status: false,
-        });
-      } else {
-        res.status(200).json({
-          message: "TeacherCourseData was inserted successfully",
-        });
-      }
-    });
+  const tempTeacherCourseData = new teacherCourseData(req.body);
+  tempTeacherCourseData.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: err,
+        status: false,
+      });
+    } else {
+      res.status(200).json({
+        message: "TeacherCourseData was inserted successfully",
+      });
+    }
   });
+});
 
-  router.get("/allCourses", async (req, res) => {
-    await teacherCourseData.find({}, (err, data) => {
-      if (err) {
-        res.status(500).json({
-          error: err,
-          status: false,
-        });
-      } else {
-        res.status(200).json({
-          result: data,
-          message: "TeacherCourseData was get successfully",
-        });
-      }
-    });
+router.get("/allCourses", async (req, res) => {
+  await teacherCourseData.find({}, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        error: err,
+        status: false,
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: "TeacherCourseData was get successfully",
+      });
+    }
   });
+});
 
-  module.exports = router;
+module.exports = router;
